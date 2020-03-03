1ed15e4139aaa50d6d9eaa0fe1e4b7d4f52447e2
Maria <mabellxd16@gmail.com>
2020-03-03 17:35:47 -0500

    added meme v2

--- Meme v2/sketch.js
+++ Meme v2/sketch.js
@@ -2,9 +2,11 @@
 
 // global scope
 var catImage;
+var guyImage;
 
 function preload(){
 	catImage = loadImage("cat.jpg");
+	guyImage = loadImage("guy.jpg");
 
 }
 
@@ -18,26 +20,30 @@ function draw() {
 	background(165, 118, 232, 91);
 
 	// draw the image
-	image(catImage, 0, 0, 500,500);
+	if (mouseIsPressed){
+		image(guyImage, 0, 0, width, height);
+	} else {
+		image(catImage, 0, 0, width, height);
+	}
+	
 
-	var str = "When you didn't do your homework and you have to present it in 5 minutes";
+	var paragraph = "When you didn't do your homework and you have to present it in 5 minutes";
 	var w = textWidth(str);
 	var x = 0;
 	var y = 250;
 
-	textSize(15);
-	fill("pink");
-	textStyle(ITALIC);
-	textFont('Arial');
-	textAlign(LEFT,CENTER);
-	text(str, x, y);
+	textSize(25);
+	fill("white");
+	textFont('Verdana');
+	textAlign(CENTER);
+	text(paragraph, x, y - 230, 500, 350);
 	//stroke('pink');
 	//line(x + 10, y - -30, x + w + 250, y + 30);
 	y+=100;
 
-	textSize(30);
-	stroke('green');
-	strokeWeight(5);
+	textSize(50);
+	//stroke('green');
+	//strokeWeight(5);
 	textStyle(BOLD);
 	textAlign(CENTER, TOP);
 	textFont('Verdana');
@@ -45,5 +51,5 @@ function draw() {
 	
 	var welcome = "HELP!" ;
 	var end = map(frameCount, 0, 200, 0, welcome.length);
-	text(welcome.substring(0, end), 250, 350);
+	text(welcome.substring(0, end), 395, 350);
 }
