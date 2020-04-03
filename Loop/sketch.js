ebcb4f6437088a1ed9b4d458b1235dc1885f6aeb
Maria282 <sarmientomaria@newcomershs.org>
2020-04-02 21:00:34 -0400

    added colors to the loop

--- Loop/sketch.js
+++ Loop/sketch.js
@@ -11,19 +11,61 @@ loop example
 */
 
 function setup() {
-	createCanvas( 640, 360);
+	createCanvas( 640, 460);
 	noStroke();
-}
+	rectMode(CENTER);
+
+	}
+
 
 function draw() {
-	background(140, 171, 237, 93);
+	background("black");
 	
 
-	for (let x = 110; x <= 500; x += 100) {
-		
-		ellipse(x, 100, 50)
+	for (let x = 50; x < width; x += 50) {
+		var r = random(0, 255);
+		var g = random(0, 100);
+		var b = random(100, 255);
+		fill(r, g, b);
 
+		ellipse(x, height/2, 40);
+		fill("white");
+		ellipse(x - 10, height/2 - 10, 10);
+		ellipse(x + 10, height/2 - 10, 10);
+		
+        rect(x, height/2 + 10, 30, 10, 5);
 		fill(255, 174, 128, 100);
+		stroke(2);
+
+		var r = random(100, 100);
+		var g = random(0, 255);
+		var b = random(0, 100);
+		fill(r, g, b);
+
+		ellipse(x, height/1.3, 10);
+
+
+		var r = random(100, 100);
+		var g = random(0, 255);
+		var b = random(0, 100);
+		fill(r, g, b);
+
+
+		var r = random(100, 100);
+		var g = random(0, 255);
+		var b = random(0, 100);
+		fill(r, g, b);
+
+		ellipse(x, height/5, 10);
+
+
+		var r = random(100, 100);
+		var g = random(0, 255);
+		var b = random(0, 100);
+		fill(r, g, b);
+
+
+		
 		
 
 
