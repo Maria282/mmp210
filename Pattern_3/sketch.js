3ac78dc026cb9b411f9c793466d9d0d279b1c0c9
Maria282 <sarmientomaria@newcomershs.org>
2020-04-25 19:21:38 -0400

    added colors

--- Pattern_3/sketch.js
+++ Pattern_3/sketch.js
@@ -3,63 +3,87 @@ Pattern #3
 4.24.2020
 */
 
-var wSlider;
-var heightSlider;
+var bgSlider;
+var greenSlider;
 
 function setup() {
-	createCanvas(600, 600);
+	createCanvas(500, 500);
 	noStroke();
 
-	createP('Pixel Size');
-	wSlider = createSlider(1, 100, 40);
-	wSlider.input(pattern);
 
-	createP('Height');
-	heightSlider = createSlider(-10, height, 300);
-	heightSlider.input(pattern);
+	input = createInput('How you feel?');
+	input.position(690, 270);
 
+	button = createButton('submit');
+	button.position(input.x + input.width, 270);
+	button.mousePressed(pattern);
 	
+	createP('Background Color');
+	bgSlider = createSlider(0, 255, 61);
+	bgSlider.position(875, 840)
+	bgSlider.input(pattern);
+
+	createP('Green Color');
+	greenSlider = createSlider(0, 250, 154);
+	greenSlider.position(875, 890)
+	greenSlider.input(pattern);
 
 	var patternButton = createButton('New Pattern');
+	patternButton.position(690, 755);
 	patternButton.mousePressed(pattern);
 
 	var saveButton = createButton('Save');
+	saveButton.position(780, 755);
 	saveButton.mousePressed(saveImage);
 
-	
-
-
 	pattern();
-
 }
 
-function saveImage(){
+function saveImage() {
 	save('pattern.png');
 }
 
 
 function pattern() {
-	background(51);
+	background(bgSlider.value());
 
-	var w = wSlider.value();  //width of one cell
-	var h = w;    // height of one cell
+	var w = 30;
+	var h = w;
 
-	for (let x = 0; x <= width; x += w) {
-		for (let y = 0; y <= height; y += h) {
+	for (let x = 0; x <= width; x += w){
+		for (let y = 0; y <= height; y += h){
+			push();
+			translate(x, y);
 
-			let r = random (100, 250);  // red
-			let g = random( 0, 100); // green
-			let b = random (100, 155); // blue
+            var g = greenSlider.value(); // green
+            var r = random(55, 155);
+            var b = random(100, 150);
 
-			if (y > heightSlider.value()) {
-				r += random(100, 200);
-				g += random(50, 150);
-			}
+			
+            stroke(r, g, b);
+            strokeWeight(2);
 
-			fill(r, g, b);
-			rect(x + random(10), y + random(10), w + random(20), h + random(100));
+            var randomChoice = random(4);
+			if (randomChoice < 1) {
+				line(0, 0, w, h);
+			} else if (randomChoice < 2){
+				 line(w, 0, 0, h);
+			} else if (randomChoice < 3) {
+				line(0, 0, w, h);
+			} else if (randomChoice < 4) {
+				line(w, 0, w, 0);
+			}	
+            
+            pop();
 
+            textAlign(CENTER, CENTER);
+            textSize(70);
+            fill('black');
+            stroke('white');
+            strokeWeight(20);
+            text(input.value(), width/2, height/2);
+
+
+		}
 	}
-}
-	
 }
